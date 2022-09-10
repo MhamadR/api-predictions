10280e2a22d69e5afa75091ce6bf7100e42c5da4
MhamadR <mhamadraslan@gmail.com>
2022-09-10 23:51:20 +0300

    Added functions to fetch data and display dog pictures

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
+window.onload = loadByBreed("affenpinscher");
 async function fetchBreedData() {
   try {
     const response = await fetch("https://dog.ceo/api/breeds/list/all");
     const data = await response.json();
-    console.log(data.message);
+    createBreedList(data.message);
   } catch (e) {
     console.log("There was a problem fetching the breed list.");
   }
 }
 
 fetchBreedData();
+
+function createBreedList(breedList) {
+  document.getElementById("breedListContainer").innerHTML = `
+  <select onchange="loadByBreed(this.value)">
+        ${Object.keys(breedList)
+          .map(function (breed) {
+            return `<option>${breed}</option>`;
+          })
+          .join("")}
+      </select>
+  `;
+}
+
+async function loadByBreed(breed) {
+  const response = await fetch(
+    `https://dog.ceo/api/breed/${breed}/images/random`
+  );
+  const data = await response.json();
+  const randBreedPic = data.message;
+  document.getElementById(
+    "breedPic"
+  ).style.backgroundImage = `url(${randBreedPic})`;
+}
